69fc3abffa46daf190d0b27b5f626350cb3f89c9
Sitesh Roy <siteshroy@Siteshs-MacBook-Air.local>
2025-07-23 16:58:04 +0530

    get set added

--- app/main.js
+++ app/main.js
@@ -5,20 +5,42 @@ console.log("Logs from your program will appear here!");
 
 const simpleDataTypes = ["+", "-", ":", "#", "("];
 const aggregateDataTypes = ["*", "$"];
+const globalMap = {};
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
     const command = data.toString().trim().split("\r\n");
     let commandLength = 0;
+    let i = 0;
     if (command[0].substring(0, 1) === "*") {
       commandLength = parseInt(command[0].substring(1), 10);
+      i = 2;
     }
-    let i = 0;
-    if (command[2].toLowerCase() === "ping") {
-      connection.write("+PONG\r\n");
-    } else if (command[2].toLowerCase() === "echo") {
-      connection.write(`+${command[4]}\r\n`);
+    while (i < command.length) {
+      if (command[i].toLowerCase() === "ping") {
+        connection.write("+PONG\r\n");
+        i++;
+      } else if (command[i].toLowerCase() === "echo") {
+        connection.write(`+${command[i + 2]}\r\n`);
+        i += 2;
+      } else if (command[i].toLowerCase() === "set") {
+        const key = command[i + 1];
+        const value = command[i + 2];
+        globalMap[key] = value;
+        connection.write(`+OK\r\n`);
+        i += 3;
+      } else if (command[i].toLowerCase() === "get") {
+        const key = command[i + 1];
+        if (globalMap[key]) {
+          connection.write(
+            `$${globalMap[key].length}\r\n${globalMap[key]}\r\n`
+          );
+        } else {
+          connection.write("$-1\r\n");
+        }
+        i += 2;
+      }
     }
   });
 
